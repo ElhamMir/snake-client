d5891bdfca5b46cfd77ff2017c07d08acbd9896f
Elham <mireshghi@yahoo.com>
2021-11-03 23:52:35 +0000

    added the function to detect a key

--- play.js
+++ play.js
@@ -1,8 +1,30 @@
 const { connect } = require('./client');
 
+// setup interface to handle user input from stdin
+const handleUserInput = function (key) {
+    if(key === "a"){
+        process.exit()
 
-// establishes a connection with the game server
+    }
+    // your code here
+  };
+  //var x = key;
 
+// If the pressed keyboard button is "a" or "A" (using caps lock or shift), alert some text.
+
+
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", key=> {handleUserInput(key)});
+    return stdin;
+  };
+  
+  
+  setupInput()
 
 console.log("Connecting ...");
 connect();
+module.exports = {setupInput}
