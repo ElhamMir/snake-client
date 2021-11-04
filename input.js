ae7932681719444ff3fc144670c26fe55f6cb9d2
Elham <mireshghi@yahoo.com>
2021-11-04 00:17:47 +0000

    added all keys

--- input.js
+++ input.js
@@ -1,7 +1,23 @@
-const handleUserInput = function (key) {
-    if(key === "a"){
+// Stores the active TCP connection object.
+let connection;
+
+const handleUserInput = function (conn,key) {
+    
+    if(key === "b"){
         process.exit()
 
+    }
+    else if(key === "w"){
+        conn.write("Move: up")
+    }
+    else if(key === "a"){
+        console.log("Move: left")
+    }
+    else if(key === "s"){
+        console.log("Move: down")
+    }
+    else if(key === "d"){
+        console.log("Move: right")
     }
     // your code here
   };
@@ -10,7 +26,8 @@ const handleUserInput = function (key) {
 // If the pressed keyboard button is "a" or "A" (using caps lock or shift), alert some text.
 
 
-const setupInput = function () {
+const setupInput = function (conn) {
+    connection = conn;
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
